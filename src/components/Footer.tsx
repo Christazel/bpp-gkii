70ae196f437dc4ceb38a1c50a10a33f33c36fd47
Yohan Christazel Jeffry <yohan.christazel9@gmail.com>
2026-09-10 09:23:35 +0700

    feat(faq): add interactive FAQ accordion section inspired by JelajahKode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -99,6 +99,11 @@ export default function Footer() {
                     Warta & Siaran Pers BPP
                   </a>
                 </li>
+                <li>
+                  <a href="#faq" className="hover:text-white transition-colors inline-block">
+                    Pusat Bantuan / FAQ
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
